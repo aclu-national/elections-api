3a67db9a8e6fa92a117699aa0b2efbadd6c6f649
Dan Phiffer <dan@phiffer.org>
2018-05-18 16:34:21 -0400

    :sparkles: server stuff

---
+++ scripts/setup_ubuntu.sh
@@ -0,0 +1,61 @@
+#!/bin/env bash
+
+# Before running this:
+#
+# mkdir /usr/local/aclu
+# chown ubuntu:ubuntu /usr/local/aclu
+# cd /usr/local/aclu
+# git clone git@github.com:aclu-national/election-data.git
+# cd election-data
+# sudo scripts/setup_ubuntu.sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with admin privs: sudo ./setup_ubuntu.sh"
+  exit
+fi
+
+PROJECT="election-data"
+PROJECT_PATH="/usr/local/aclu/$PROJECT"
+POSTGRES_VERSION="9.5"
+POSTGRES_MAIN="/etc/postgresql/$POSTGRES_VERSION/main"
+
+apt update
+apt upgrade -y
+
+apt install -y fail2ban ufw htop emacs24-nox postgresql postgresql-contrib \
+               build-essential gdal-bin python python-pip python-gevent
+
+ufw allow 80
+ufw allow 22
+yes | ufw enable
+
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+
+apt update
+apt install -y postgis
+
+pip install --upgrade pip
+pip install flask flask_cors psycopg2-binary gunicorn bs4 urllib3 certifi arrow
+
+if [ -f "$POSTGRES_MAIN/postgresql.conf" ] ; then
+	mv "$POSTGRES_MAIN/postgresql.conf" "$POSTGRES_MAIN/postgresql.conf.bak"
+fi
+
+cp "$PROJECT_PATH/server/postgresql.conf" "$POSTGRES_MAIN/postgresql.conf"
+sed -i -e 's/\(local\s*all\s*postgres\s*\)peer/\1trust/' "$POSTGRES_MAIN/pg_hba.conf"
+
+service postgresql restart
+
+sudo -u postgres createuser --superuser ubuntu
+sudo -u postgres createdb us_congress
+sudo -u postgres psql -d us_congress -c "CREATE EXTENSION postgis;"
+
+cd "$PROJECT_PATH"
+make
+
+cp "$PROJECT_PATH/server/elections.service" /etc/systemd/system/
+cp "$PROJECT_PATH/server/elections.socket" /etc/systemd/system/
+systemctl enable elections.service
+service elections start
+
+echo "done"
---
+++ server/gunicorn.py
@@ -0,0 +1,11 @@
+# http://gunicorn-docs.readthedocs.org/en/latest/configure.html#configuration-file
+# -*-python-*-
+
+import multiprocessing, os
+
+os.environ['POSTGRES_DSN'] = 'user=postgres dbname=elections'
+
+bind = "0.0.0.0:%s" % os.getenv('PORT', 80)
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "gunicorn.workers.ggevent.GeventWorker"
+pidfile = "/run/gunicorn/pid"

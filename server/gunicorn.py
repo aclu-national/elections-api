2662acb8325190068c79708ad40801b23573209b
Dan Phiffer <dan@phiffer.org>
2018-05-22 14:59:19 +0000

    :wrench: tweaks to server settings

--- server/gunicorn.py
+++ server/gunicorn.py
@@ -5,7 +5,7 @@ import multiprocessing, os
 
 os.environ['POSTGRES_DSN'] = 'user=postgres dbname=elections'
 
-bind = "0.0.0.0:%s" % os.getenv('PORT', 80)
+bind = "0.0.0.0:%s" % os.getenv('PORT', 5000)
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gunicorn.workers.ggevent.GeventWorker"
 pidfile = "/run/gunicorn/pid"

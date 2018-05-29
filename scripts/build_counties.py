f8665908f2c07e8a2b954a4c71d39041f54af676
Dan Phiffer <dan@phiffer.org>
2018-05-29 11:24:35 +0200

    :zap: make counties

---
+++ scripts/build_counties.py
@@ -0,0 +1,49 @@
+#!/bin/env python
+
+import json, os, sys, re, us, area
+import mapzen.whosonfirst.geojson
+import mapzen.whosonfirst.utils
+
+script = os.path.realpath(sys.argv[0])
+scripts_dir = os.path.dirname(script)
+root_dir = os.path.dirname(scripts_dir)
+
+path = "%s/sources/counties/counties.geojson" % root_dir
+
+encoder = mapzen.whosonfirst.geojson.encoder(precision=None)
+
+print("Loading %s" % path)
+with open(path) as data_file:
+	data = json.load(data_file)
+
+for feature in data["features"]:
+
+	props = feature["properties"]
+	state_fips = props["STATEFP"]
+	state = us.states.lookup(state_fips).abbr
+	state = str(state).lower()
+
+	slug = re.sub(r"[^a-zA-Z]+", '-', props["NAME"]).lower()
+	name = "county_%s_%s" % (state, slug)
+	path = "%s/data/counties/%s/%s.geojson" % (root_dir, state, name)
+
+	print("Saving %s" % path)
+	feature["properties"] = {
+		"state": state,
+		"name": props["NAME"],
+		"area_land": props["ALAND"],
+		"area_water": props["AWATER"],
+		"fips_id": props["GEOID"]
+	}
+	feature["id"] = name
+
+	mapzen.whosonfirst.utils.ensure_bbox(feature)
+
+	dirname = os.path.dirname(path)
+	if not os.path.exists(dirname):
+		os.makedirs(dirname)
+
+	with open(path, 'w') as outfile:
+		encoder.encode_feature(feature, outfile)
+
+print("Done")

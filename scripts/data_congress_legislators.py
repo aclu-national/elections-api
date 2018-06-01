22639f766f86b4c5e8f208199b27308a7384447e
Dan Phiffer <dan@phiffer.org>
2018-06-01 17:13:45 -0400

    :dizzy: assign aclu_ids to congress_legislators

--- scripts/data_congress_legislators.py
+++ scripts/data_congress_legislators.py
@@ -1,44 +1,85 @@
 #!/bin/env python
 
-import os, re, sys, yaml, json
+import os, re, sys, yaml, json, unicodedata
+import data_index
 
 script = os.path.realpath(sys.argv[0])
 scripts_dir = os.path.dirname(script)
 root_dir = os.path.dirname(scripts_dir)
 
-legislators = {}
+legislator_lookup = {}
 
-path = "%s/sources/legislators/legislators-current.yaml" % root_dir
-print("Loading %s" % path)
-file = open(path, "r")
+source_path = "%s/sources/congress_legislators/legislators-current.yaml" % root_dir
+print("Loading %s" % source_path)
+file = open(source_path, "r")
 data = yaml.load(file)
 
 for legislator in data:
 	id = legislator["id"]["bioguide"]
-	legislators[id] = legislator
+	legislator_lookup[id] = legislator
 
-path = "%s/sources/legislators/legislators-social-media.yaml" % root_dir
-print("Loading %s" % path)
-file = open(path, "r")
+source_path = "%s/sources/congress_legislators/legislators-social-media.yaml" % root_dir
+print("Loading %s" % source_path)
+file = open(source_path, "r")
 data = yaml.load(file)
+
+legislator_list = []
 
 for legislator in data:
 	id = legislator["id"]["bioguide"]
-	legislators[id]["social"] = legislator["social"]
+	legislator_lookup[id]["social"] = legislator["social"]
+	legislator_list.append(legislator_lookup[id])
 
-for id in legislators:
+def strip_accents(s):
+	if (type(s) == str):
+		s = s.decode('utf-8')
+	return ''.join(c for c in unicodedata.normalize('NFD', s)
+		if unicodedata.category(c) != 'Mn')
 
-	legislator = legislators[id]
-	name = "%s.json" % id
-	path = "%s/data/legislators/%s" % (root_dir, name)
+def get_filename(legislator):
+	fname = strip_accents(legislator["name"]["first"])
+	lname = strip_accents(legislator["name"]["last"])
+	name = "%s-%s" % (fname, lname)
+	slug = re.sub('[^A-Za-z]+', '-', name).lower()
+	state = legislator["terms"][0]["state"].lower()
+	filename = "congress_legislator_%s_%s.json" % (state, slug)
+	return filename
 
-	print("Saved %s" % path)
+def sort_legislators(a, b):
+	a_filename = get_filename(a)
+	b_filename = get_filename(b)
+	if a_filename > b_filename:
+		return 1
+	else:
+		return -1
 
-	dirname = os.path.dirname(path)
+legislator_list.sort(sort_legislators)
+
+for legislator in legislator_list:
+
+	filename = get_filename(legislator)
+	state = legislator["terms"][0]["state"].lower()
+	fname = legislator["name"]["first"]
+	lname = legislator["name"]["last"]
+	name = "%s %s" % (fname, lname)
+
+	path = "congress_legislators/%s/%s" % (state, filename)
+	abs_path = "%s/data/%s" % (root_dir, path)
+
+	aclu_id = data_index.get_id('elections-api', 'congress_legislator', path, name)
+
+	legislator["id"]["aclu_id"] = aclu_id
+
+	print("Saving %s" % path)
+
+	dirname = os.path.dirname(abs_path)
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 
-	with open(path, 'w') as outfile:
-		json.dump(legislator, outfile, indent=4, sort_keys=True)
+	with open(abs_path, 'w') as outfile:
+		json.dump(legislator, outfile, indent=2, sort_keys=True)
+
+print("Saving index")
+data_index.save_index('elections-api')
 
 print("Done")

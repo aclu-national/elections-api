0e8ae9b1d4ad88068129d90007b02979c22b44c4
Dan Phiffer <dan@phiffer.org>
2018-06-01 16:38:41 -0400

    :dizzy: add aclu_ids to congress_districts_113

--- scripts/data_congress_districts_tiger.py
+++ scripts/data_congress_districts_tiger.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
 import json, os, sys, us, area, optparse, re
-import postgres_db
+import postgres_db, data_index
 import mapzen.whosonfirst.geojson
 import mapzen.whosonfirst.utils
 
@@ -16,12 +16,12 @@ options, args = opt_parser.parse_args()
 
 session = options.start
 district_prop = "CD%sFP" % session
-path = "%s/sources/districts_%s/districts_%s.geojson" % (root_dir, session, session)
+source_path = "%s/sources/congress_districts_%s/congress_districts_%s.geojson" % (root_dir, session, session)
 
 conn = postgres_db.connect()
 cur = conn.cursor()
 
-cur.execute("SELECT id, start_date, end_date FROM sessions")
+cur.execute("SELECT id, start_date, end_date FROM congress_sessions")
 rs = cur.fetchall()
 sessions = {}
 if rs:
@@ -34,24 +34,50 @@ if rs:
 
 encoder = mapzen.whosonfirst.geojson.encoder(precision=None)
 
-print("Loading %s" % path)
-with open(path) as data_file:
+def get_filename(props):
+
+	global district_prop
+
+	state_geoid = props["STATEFP"]
+	state_upper = str(us.states.lookup(state_geoid).abbr)
+	state = state_upper.lower()
+
+	district_num = props[district_prop]
+	filename = "congress_district_%s_%s_%s.geojson" % (session, state, district_num)
+	return filename
+
+def sort_districts(a, b):
+	a_filename = get_filename(a['properties'])
+	b_filename = get_filename(b['properties'])
+	if a_filename > b_filename:
+		return 1
+	else:
+		return -1
+
+print("Loading %s" % source_path)
+with open(source_path) as data_file:
 	data = json.load(data_file)
+
+data["features"].sort(sort_districts)
 
 for feature in data["features"]:
 
 	props = feature["properties"]
+
 	if props[district_prop] == "ZZ":
 		continue
 
 	state_geoid = props["STATEFP"]
-	state = us.states.lookup(state_geoid).abbr
-	state = str(state).lower()
-
+	state_upper = str(us.states.lookup(state_geoid).abbr)
+	state = state_upper.lower()
 	district_num = props[district_prop]
-	id = "district_%s_%s_%s" % (session, state, district_num)
-	name = "%s.geojson" % id
-	path = "%s/data/districts_%s/%s/%s" % (root_dir, session, state, name)
+
+	filename = get_filename(props)
+	path = "congress_districts_%s/%s/%s" % (session, state, filename)
+	abs_path = "%s/data/%s" % (root_dir, path)
+	name = "%s %s" % (state_upper, props["NAMELSAD"])
+
+	aclu_id = data_index.get_id('elections-api', 'congress_district', path, name)
 
 	non_zero_padded = re.search('^0+(\d+)', district_num)
 	if not non_zero_padded:
@@ -64,8 +90,10 @@ for feature in data["features"]:
 	print("Saving %s" % path)
 
 	feature["properties"] = {
+		"aclu_id": aclu_id,
 		"geoid": props["GEOID"],
 		"ocd_id": ocd_id,
+		"name": name,
 		"state": state,
 		"start_session": options.start,
 		"start_date": sessions[options.start]["start_date"],
@@ -73,16 +101,19 @@ for feature in data["features"]:
 		"end_date": sessions[options.end]["end_date"],
 		"district_num": district_num
 	}
-	feature["id"] = id
+	feature["id"] = aclu_id
 
 	mapzen.whosonfirst.utils.ensure_bbox(feature)
 	feature["properties"]["area"] = area.area(feature["geometry"])
 
-	dirname = os.path.dirname(path)
+	dirname = os.path.dirname(abs_path)
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 
-	with open(path, 'w') as outfile:
+	with open(abs_path, 'w') as outfile:
 		encoder.encode_feature(feature, outfile)
+
+print("Saving index")
+data_index.save_index('elections-api')
 
 print("Done")

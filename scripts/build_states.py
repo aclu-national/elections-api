cee4d92ece81bd800266198949c61b8dd7caedab
Dan Phiffer <dan@phiffer.org>
2018-06-01 13:55:25 +0000

    :card_index: assign aclu_id to states

--- scripts/build_states.py
+++ scripts/build_states.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
-import json, os, sys, us, area
+import os, sys, json, us, area
+import data_index
 import mapzen.whosonfirst.geojson
 import mapzen.whosonfirst.utils
 
@@ -16,33 +17,48 @@ print("Loading %s" % path)
 with open(path) as data_file:
 	data = json.load(data_file)
 
+def sort_states(a, b):
+	if a['properties']['STUSPS'] > b['properties']['STUSPS']:
+		return 1
+	else:
+		return -1
+
+data["features"].sort(sort_states)
+
 for feature in data["features"]:
 
 	props = feature["properties"]
 	state = props["STUSPS"].lower()
+	name = props["NAME"]
 
-	name = "state_%s.geojson" % state
-	path = "%s/data/states/%s" % (root_dir, name)
+	filename = "state_%s.geojson" % state
+	path = "states/%s" % filename
+	abs_path = "%s/data/%s" % (root_dir, path)
 	ocd_id = 'ocd-division/country:us/state:%s' % state
+	aclu_id = data_index.get_id('elections-api', 'state', path, name)
 
 	print("Saving %s" % path)
 	feature["properties"] = {
+		"aclu_id": aclu_id,
 		"geoid": props["STATEFP"],
 		"ocd_id": ocd_id,
 		"state": state,
-		"name": props["NAME"],
+		"name": name,
 		"area_land": props["ALAND"],
 		"area_water": props["AWATER"]
 	}
-	feature["id"] = name
+	feature["id"] = aclu_id
 
 	mapzen.whosonfirst.utils.ensure_bbox(feature)
 
-	dirname = os.path.dirname(path)
+	dirname = os.path.dirname(abs_path)
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 
-	with open(path, 'w') as outfile:
+	with open(abs_path, 'w') as outfile:
 		encoder.encode_feature(feature, outfile)
+
+print("Saving index")
+data_index.save_index('elections-api')
 
 print("Done")

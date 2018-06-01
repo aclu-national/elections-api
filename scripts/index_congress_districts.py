a1ddcb35e3dbe723d31b43c639198f76b51dc1fc
Dan Phiffer <dan@phiffer.org>
2018-06-01 17:23:23 -0400

    :card_index: update congress_districts indexing

--- scripts/index_congress_districts.py
+++ scripts/index_congress_districts.py
@@ -11,9 +11,10 @@ conn = postgres_db.connect()
 cur = conn.cursor()
 
 cur.execute("DROP TABLE IF EXISTS districts CASCADE")
+cur.execute("DROP TABLE IF EXISTS congress_districts CASCADE")
 cur.execute('''
-	CREATE TABLE districts (
-		id SERIAL PRIMARY KEY,
+	CREATE TABLE congress_districts (
+		aclu_id VARCHAR(255) PRIMARY KEY,
 		geoid VARCHAR(255),
 		ocd_id VARCHAR(255),
 		name VARCHAR(255),
@@ -29,7 +30,8 @@ cur.execute('''
 conn.commit()
 
 insert_sql = '''
-	INSERT INTO districts (
+	INSERT INTO congress_districts (
+		aclu_id,
 		geoid,
 		ocd_id,
 		name,
@@ -39,16 +41,16 @@ insert_sql = '''
 		district_num,
 		boundary,
 		area
-	) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+	) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 '''
 
 directories = []
-for state in os.listdir("%s/data/districts_113" % root_dir):
+for state in os.listdir("%s/data/congress_districts_113" % root_dir):
 	if state.startswith("."):
 		continue
-	directories.append("%s/data/districts_113/%s" % (root_dir, state))
+	directories.append("%s/data/congress_districts_113/%s" % (root_dir, state))
 
-directories.append("%s/data/districts_116_pa" % root_dir)
+directories.append("%s/data/congress_districts_116_pa" % root_dir)
 directories.sort()
 
 for dir in directories:
@@ -64,7 +66,7 @@ for dir in directories:
 	files.sort()
 	for filename in files:
 
-		regex = '^district_(\d+)_([a-z][a-z])_(\d+)\.geojson$'
+		regex = '^congress_district_(\d+)_([a-z][a-z])_(\d+)\.geojson$'
 		matches = re.search(regex, filename)
 		if matches == None:
 			print("skipping %s" % filename)
@@ -76,9 +78,10 @@ for dir in directories:
 		with open(path) as geojson:
 			feature = json.load(geojson)
 
+		aclu_id = feature["properties"]["aclu_id"]
 		geoid = feature["properties"]["geoid"]
 		ocd_id = feature["properties"]["ocd_id"]
-		name = feature["id"]
+		name = feature["properties"]["name"]
 		state = feature["properties"]["state"]
 		start_session = int(feature["properties"]["start_session"])
 		end_session = int(feature["properties"]["end_session"])
@@ -88,6 +91,7 @@ for dir in directories:
 		area = float(feature["properties"]["area"])
 
 		district = [
+			aclu_id,
 			geoid,
 			ocd_id,
 			name,
@@ -104,30 +108,30 @@ for dir in directories:
 
 print("Marking excess at-large districts")
 cur.execute('''
-	UPDATE districts
+	UPDATE congress_districts
 	SET at_large_only = 'Y'
 	WHERE district_num = 0 AND (
-		SELECT count(id)
-		FROM districts AS d
-		WHERE d.start_session <= districts.end_session
-		  AND d.end_session >= districts.start_session
-		  AND d.state = districts.state
+		SELECT count(aclu_id)
+		FROM congress_districts AS d
+		WHERE d.start_session <= congress_districts.end_session
+		  AND d.end_session >= congress_districts.start_session
+		  AND d.state = congress_districts.state
 	) = 1
 ''')
 
 print("Indexing postgis geometry")
 cur.execute('''
-	UPDATE districts
+	UPDATE congress_districts
 	SET boundary_geom = ST_SetSRID(ST_GeomFromGeoJSON(boundary), 4326)
 ''')
 cur.execute('''
-	CREATE INDEX districts_boundary_gix ON districts USING GIST (boundary_geom)
+	CREATE INDEX districts_boundary_gix ON congress_districts USING GIST (boundary_geom)
 ''')
 conn.commit()
 
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 cur.execute('''
-	VACUUM ANALYZE districts
+	VACUUM ANALYZE congress_districts
 ''')
 
 conn.close()

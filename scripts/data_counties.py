0a57edd8aa9bc1bf60c6f67d7ece4a91940f09c4
Dan Phiffer <dan@phiffer.org>
2018-06-04 13:36:05 -0400

    :dizzy: assign aclu_ids to counties

--- scripts/data_counties.py
+++ scripts/data_counties.py
@@ -3,6 +3,7 @@
 import json, os, sys, re, us, area
 import mapzen.whosonfirst.geojson
 import mapzen.whosonfirst.utils
+import data_index
 
 script = os.path.realpath(sys.argv[0])
 scripts_dir = os.path.dirname(script)
@@ -16,21 +17,44 @@ print("Loading %s" % path)
 with open(path) as data_file:
 	data = json.load(data_file)
 
-for feature in data["features"]:
-
-	props = feature["properties"]
+def get_filename(props):
+	slug = re.sub(r"[^a-zA-Z]+", '-', props["NAME"]).lower()
 	state_geoid = props["STATEFP"]
 	state = us.states.lookup(state_geoid).abbr
 	state = str(state).lower()
+	filename = "county_%s_%s.geojson" % (state, slug)
+	return filename
 
+def sort_counties(a, b):
+	a_filename = get_filename(a['properties'])
+	b_filename = get_filename(b['properties'])
+	if (a_filename > b_filename):
+		return 1
+	else:
+		return -1
+
+data["features"].sort(sort_counties)
+
+for feature in data["features"]:
+
+	props = feature["properties"]
+
+	state_geoid = props["STATEFP"]
+	state = us.states.lookup(state_geoid).abbr
+	state = str(state).lower()
 	slug = re.sub(r"[^a-zA-Z]+", '-', props["NAME"]).lower()
 	ocd_slug = re.sub(r"[^a-zA-Z]+", '_', props["NAME"]).lower()
-	name = "county_%s_%s" % (state, slug)
-	path = "%s/data/counties/%s/%s.geojson" % (root_dir, state, name)
+
+	filename = get_filename(props)
+	path = "counties/%s/%s" % (state, filename)
+	abs_path = "%s/data/%s" % (root_dir, path)
+
+	aclu_id = data_index.get_id('elections-api', 'county', path, props["NAME"])
 	ocd_id = 'ocd-division/country:us/state:%s/county:%s' % (state, ocd_slug)
 
 	print("Saving %s" % path)
 	feature["properties"] = {
+		"aclu_id": aclu_id,
 		"geoid": props["GEOID"],
 		"ocd_id": ocd_id,
 		"state": state,
@@ -38,15 +62,18 @@ for feature in data["features"]:
 		"area_land": props["ALAND"],
 		"area_water": props["AWATER"]
 	}
-	feature["id"] = name
+	feature["id"] = aclu_id
 
 	mapzen.whosonfirst.utils.ensure_bbox(feature)
 
-	dirname = os.path.dirname(path)
+	dirname = os.path.dirname(abs_path)
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 
-	with open(path, 'w') as outfile:
+	with open(abs_path, 'w') as outfile:
 		encoder.encode_feature(feature, outfile)
+
+print("Saving index")
+data_index.save_index('elections-api')
 
 print("Done")

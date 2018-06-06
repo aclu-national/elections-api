8e0c67ee813670af7e37c68221a55d658b300480
Dan Phiffer <dan@phiffer.org>
2018-06-06 12:35:58 -0400

    :zap: make congress_photos

---
+++ scripts/source_congress_photos.py
@@ -0,0 +1,23 @@
+#/bin/env python
+
+import os, re, sys, yaml
+from subprocess import call
+
+script = os.path.realpath(sys.argv[0])
+scripts_dir = os.path.dirname(script)
+root_dir = os.path.dirname(scripts_dir)
+
+source_path = "%s/sources/congress_legislators/legislators-current.yaml" % root_dir
+print("Loading %s" % source_path)
+file = open(source_path, "r")
+data = yaml.load(file)
+
+for legislator in data:
+	bioguide = legislator["id"]["bioguide"]
+	url = "http://bioguide.congress.gov/bioguide/photo/%s/%s.jpg" % (bioguide[0], bioguide)
+	path = "%s/sources/congress_photos/%s.jpg" % (root_dir, bioguide)
+	cmd = ["/usr/bin/curl", "-s", "-o", path, url]
+	print("Downloading %s" % url)
+	call(cmd)
+
+print("Done")

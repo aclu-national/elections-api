f9e99c4e75d1b52af553bbc6a62b4ddbf1bf3a04
Dan Phiffer <dan@phiffer.org>
2018-06-22 11:44:47 -0400

    :card_index: index new election calendar CSVs

--- scripts/index_elections.py
+++ scripts/index_elections.py
@@ -10,104 +10,127 @@ root_dir = os.path.dirname(scripts_dir)
 conn = postgres_db.connect()
 cur = conn.cursor()
 
-cur.execute("DROP TABLE IF EXISTS elections CASCADE")
-cur.execute('''
-CREATE TABLE elections (
-	state CHAR(2),
-	description VARCHAR(255),
-	type VARCHAR(255),
-	election_date DATE,
-	primary_reg_deadline DATE,
-	primary_reg_mail_online_deadline DATE,
-	general_reg_deadline DATE,
-	general_reg_mail_online_deadline DATE,
-	online_url VARCHAR(255),
-	same_day VARCHAR(255),
-	absentee VARCHAR(255),
-	early VARCHAR(255),
-	early_start DATE,
-	early_end DATE,
-	vbm_apply DATE,
-	vbm_ballot_due DATE,
-	id_required VARCHAR(255),
-	sos_info_url VARCHAR(255),
-	polling_place_locator_url VARCHAR(255),
-	ballotpedia_url VARCHAR(255),
-	notes VARCHAR(255)
-)''')
-conn.commit()
-
-insert_sql = '''
-	INSERT INTO elections (
-		state,
-		description,
-		type,
-		election_date,
-		primary_reg_deadline,
-		primary_reg_mail_online_deadline,
-		general_reg_deadline,
-		general_reg_mail_online_deadline,
-		online_url,
-		same_day,
-		absentee,
-		early,
-		early_start,
-		early_end,
-		vbm_apply,
-		vbm_ballot_due,
-		id_required,
-		sos_info_url,
-		polling_place_locator_url,
-		ballotpedia_url,
-		notes
-	) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-'''
-
-fh = open('%s/sources/aclu/aclu_elections.csv' % root_dir, 'rb')
-reader = csv.reader(fh)
-
-row_num = 0
-header = []
-
 def format_date(date):
 	if date == '':
 		return None
 	else:
 		return arrow.get(date, 'M/D/YYYY').format('YYYY-MM-DD')
 
+cur.execute("DROP TABLE IF EXISTS elections CASCADE")
+cur.execute("DROP TABLE IF EXISTS election_info CASCADE")
+cur.execute('''
+CREATE TABLE election_info (
+	state CHAR(2) PRIMARY KEY,
+	online_reg_url VARCHAR(255),
+	check_reg_url VARCHAR(255),
+	polling_place_url VARCHAR(255),
+	voter_id_req VARCHAR(255),
+	same_day VARCHAR(255),
+	vote_by_mail VARCHAR(255),
+	early_voting VARCHAR(255)
+)''')
+conn.commit()
+
+insert_sql = '''
+	INSERT INTO election_info (
+		state,
+		online_reg_url,
+		check_reg_url,
+		polling_place_url,
+		voter_id_req,
+		same_day,
+		vote_by_mail,
+		early_voting
+	) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+'''
+
+fh = open('%s/sources/aclu/aclu_election_info.csv' % root_dir, 'rb')
+reader = csv.reader(fh)
+
+row_num = 0
+header = []
+
 for row in reader:
 	if row_num == 0:
 		header = row
 	else:
 
-		print("%s - %s - %s" % (row[0], row[1], format_date(row[3])))
+		state = row[0].lower()
 
-		state = us.states.lookup(unicode(row[0])).abbr.lower()
+		print("election_info - %s" % state)
+
 		values = (
 			state,
-			row[1],
 			row[2],
-			format_date(row[3]),
-			format_date(row[4]),
-			format_date(row[5]),
-			format_date(row[6]),
-			format_date(row[7]),
-			row[8],
-			row[9],
-			row[10],
-			row[11],
-			format_date(row[12]),
-			format_date(row[13]),
-			format_date(row[14]),
-			format_date(row[15]),
-			row[16],
-			row[17],
-			row[18],
-			row[19],
-			row[20]
+			row[3],
+			row[4],
+			row[5],
+			row[6],
+			row[7],
+			row[8]
 		)
 		cur.execute(insert_sql, values)
+
 	row_num = row_num + 1
 
 conn.commit()
+
+cur.execute("DROP TABLE IF EXISTS election_dates CASCADE")
+cur.execute('''
+CREATE TABLE election_dates (
+	state CHAR(2),
+	type VARCHAR(20),
+	name VARCHAR(255),
+	value DATE
+)''')
+conn.commit()
+
+insert_sql = '''
+	INSERT INTO election_dates (
+		state,
+		type,
+		name,
+		value
+	) VALUES (%s, %s, %s, %s)
+'''
+
+for type in ['primary', 'general']:
+
+	fh = open('%s/sources/aclu/aclu_election_%s.csv' % (root_dir, type), 'rb')
+	reader = csv.reader(fh)
+
+	row_num = 0
+	header = []
+
+	for row in reader:
+
+		state = row.pop(0)
+		state = state.lower()
+		state_name = row.pop(0)
+		col_num = 0
+
+		if row_num == 0:
+			header = row
+		else:
+			for value in row:
+
+				name = header[col_num]
+				if name == 'notes':
+					break
+
+				print("election_date - %s - %s - %s" % (state, type, name))
+
+				values = (
+					state,
+					type,
+					name,
+					format_date(value)
+				)
+				cur.execute(insert_sql, values)
+
+				col_num = col_num + 1
+
+		row_num = row_num + 1
+		conn.commit()
+
 print("Done")

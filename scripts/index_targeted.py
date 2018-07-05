e924fdff043f2fbae9044f7d3d38eb8323ec4d87
Dan Phiffer <dan@phiffer.org>
2018-07-05 10:56:09 -0400

    :card_index: index new targeted properties

--- scripts/index_targeted.py
+++ scripts/index_targeted.py
@@ -18,7 +18,9 @@ CREATE TABLE election_targeted_races (
 	ocd_id VARCHAR(255),
 	office VARCHAR(255),
 	summary TEXT,
-	url VARCHAR(255)
+	url VARCHAR(255),
+	link_text VARCHAR(255),
+	disclaimer VARCHAR(255)
 )''')
 
 cur.execute("DROP TABLE IF EXISTS election_targeted_initiatives CASCADE")
@@ -28,7 +30,9 @@ CREATE TABLE election_targeted_initiatives (
 	name VARCHAR(255),
 	position VARCHAR(16),
 	blurb TEXT,
-	url VARCHAR(255)
+	url VARCHAR(255),
+	link_text VARCHAR(255),
+	disclaimer VARCHAR(255)
 )''')
 
 conn.commit()
@@ -38,8 +42,10 @@ race_insert_sql = '''
 		ocd_id,
 		office,
 		summary,
-		url
-	) VALUES (%s, %s, %s, %s)
+		url,
+		link_text,
+		disclaimer
+	) VALUES (%s, %s, %s, %s, %s, %s)
 '''
 
 initiative_insert_sql = '''
@@ -48,8 +54,10 @@ initiative_insert_sql = '''
 		name,
 		position,
 		blurb,
-		url
-	) VALUES (%s, %s, %s, %s, %s)
+		url,
+		link_text,
+		disclaimer
+	) VALUES (%s, %s, %s, %s, %s, %s, %s)
 '''
 
 fh = open('%s/sources/aclu/aclu_targeted.json' % root_dir, 'rb')
@@ -57,22 +65,32 @@ data = json.load(fh)
 
 for item in data["races"]:
 	print("race - %s - %s" % (item['ocd_id'], item['office']))
+	url = None if not 'url' in item else item['url']
+	link_text = None if not 'link_text' in item else item['link_text']
+	disclaimer = None if not 'disclaimer' in item else item['disclaimer']
 	values = (
 		item['ocd_id'],
 		item['office'],
 		item['summary'],
-		item['url']
+		url,
+		link_text,
+		disclaimer
 	)
 	cur.execute(race_insert_sql, values)
 
 for item in data["initiatives"]:
 	print("ballot initiative - %s - %s" % (item['ocd_id'], item['name']))
+	url = None if not 'url' in item else item['url']
+	link_text = None if not 'link_text' in item else item['link_text']
+	disclaimer = None if not 'disclaimer' in item else item['disclaimer']
 	values = (
 		item['ocd_id'],
 		item['name'],
 		item['position'],
 		item['blurb'],
-		item['url']
+		url,
+		link_text,
+		disclaimer
 	)
 	cur.execute(initiative_insert_sql, values)
 

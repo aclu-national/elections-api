e6998d5e8a985d462ce07e0a6ae5d77b8bcd5e72
Dan Phiffer <dan@phiffer.org>
2018-10-01 16:39:44 -0400

    :sparkles: ballotpedia_candidates.csv + indexing script

---
+++ scripts/index_election_candidates.py
@@ -0,0 +1,142 @@
+#!/bin/env python
+
+import psycopg2, os, re, sys, csv, us
+import postgres_db
+
+script = os.path.realpath(sys.argv[0])
+scripts_dir = os.path.dirname(script)
+root_dir = os.path.dirname(scripts_dir)
+
+conn = postgres_db.connect()
+cur = conn.cursor()
+
+cur.execute("DROP TABLE IF EXISTS election_candidates CASCADE")
+cur.execute('''
+	CREATE TABLE election_candidates (
+		state CHAR(2),
+		name VARCHAR(255),
+		first_name VARCHAR(255),
+		last_name VARCHAR(255),
+		ballotpedia_url VARCHAR(255),
+		candidates_id INT,
+		person_id INT,
+		party VARCHAR(255),
+		race_id INT,
+		office_name VARCHAR(255),
+		office_level VARCHAR(32),
+		district_type VARCHAR(32),
+		is_incumbent BOOLEAN,
+		primary_date DATE,
+		primary_status VARCHAR(32),
+		primary_runoff_date DATE,
+		primary_runoff_status VARCHAR(32),
+		general_date DATE,
+		general_status VARCHAR(32),
+		contact_website VARCHAR(255),
+		campaign_website_url VARCHAR(255),
+		campaign_facebook_url VARCHAR(255)
+	)
+''')
+conn.commit()
+
+insert_sql = '''
+	INSERT INTO election_candidates (
+		state,
+		name,
+		first_name,
+		last_name,
+		ballotpedia_url,
+		candidates_id,
+		person_id,
+		party,
+		race_id,
+		office_name,
+		office_level,
+		district_type,
+		is_incumbent,
+		primary_date,
+		primary_status,
+		primary_runoff_date,
+		primary_runoff_status,
+		general_date,
+		general_status,
+		contact_website,
+		campaign_website_url,
+		campaign_facebook_url
+	) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+'''
+
+def valid_date(date_str):
+	if date_str == "":
+		return None
+	else:
+		return date_str
+
+def valid_bool(bool_str):
+	if bool_str == "Yes":
+		return True
+	elif bool_str == "No":
+		return False
+	else:
+		return None
+
+files = []
+
+source_dir = "%s/sources/ballotpedia" % root_dir
+path = "%s/ballotpedia_candidates.csv" % source_dir
+csvfile = open(path, 'rb')
+
+reader = csv.reader(csvfile)
+
+row_num = 0
+headers = []
+
+skipped = []
+guessed = {}
+
+empty_office_type = []
+guessed_office_type = {}
+
+def csv_row(row, headers):
+	col_num = 0
+	row_obj = {}
+	for key in headers:
+		row_obj[key] = row[col_num]
+		col_num = col_num + 1
+	return row_obj
+
+for row in reader:
+	if row_num == 0:
+		headers = row
+	else:
+		row = csv_row(row, headers)
+
+		values = (
+			row['state'].lower(),
+			row['name'],
+			row['first_name'],
+			row['last_name'],
+			row['url'], # ballotpedia_url
+			int(row['candidates_id']),
+			int(row['person_id']),
+			row['party'],
+			int(row['race_id']),
+			row['office_name'],
+			row['office_level'],
+			row['district_type'],
+			valid_bool(row['is_incumbent?']), # is_incumbent
+			valid_date(row['primary']), # primary_date
+			row['primary_status'],
+			valid_date(row['primary_runoff']), # primary_runoff_date
+			row['primary_runoff_status'],
+			valid_date(row['general']), # general_date
+			row['general_status'],
+			row['contact_website'],
+			row['campaign_website_url'],
+			row['campaign_facebook_url']
+		)
+
+		cur.execute(insert_sql, values)
+
+	row_num = row_num + 1
+conn.commit()

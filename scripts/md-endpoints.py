011b76dbedbf226271d397c01e8dc32393ad6e50
Dan Phiffer <dan@phiffer.org>
2018-11-13 17:34:24 -0500

    :nail_care: sort the endpoints/arguments

--- scripts/md-endpoints.py
+++ scripts/md-endpoints.py
@@ -5,12 +5,12 @@ import requests
 url = "https://elections.api.aclu.org/v1"
 rsp = requests.get(url).json()
 
-for path in rsp['valid_endpoints']:
+for path in sorted(rsp['valid_endpoints']):
 
 	endpoint = rsp['valid_endpoints'][path]
 	description = endpoint['description']
 
 	print("* `%s`  \n  *%s*  \n  Arguments:  " % (path, description))
 
-	for arg in endpoint['args']:
+	for arg in sorted(endpoint['args']):
 		print("    - `%s`: %s" % (arg, endpoint['args'][arg]))

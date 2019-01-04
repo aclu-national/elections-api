5785ee8083baa698423d25246478330844f857aa
Dan Phiffer <dan@phiffer.org>
2019-01-04 18:45:28 -0500

    :zap: check for which congressfolk don't have photos

--- scripts/data_congress_photos.sh
+++ scripts/data_congress_photos.sh
@@ -4,8 +4,8 @@ project="elections-api"
 project_path="/usr/local/aclu/$project"
 source_path="$project_path/sources/congress_photos"
 data_path="$project_path/data/congress_photos"
+legislators_path="$project_path/data/congress_legislators"
 
-node="/usr/local/bin/node"
 smartcrop="/usr/local/bin/smartcrop"
 width="175"
 height="175"
@@ -14,8 +14,21 @@ mkdir -p "$data_path"
 
 for path in `ls $source_path/*.jpg` ; do
 	filename=`basename $path`
-	echo "Cropping $filename"
 	cropped="$data_path/$filename"
-	json=`echo $cropped | sed 's/.jpg/.json/'`
-	$node $smartcrop --width $width --height $height $path $cropped > $json
+	if [ ! -f "$cropped" ] ; then
+		echo "Cropping $filename"
+		json=`echo $cropped | sed 's/.jpg/.json/'`
+		$smartcrop --width $width --height $height $path $cropped > $json
+	fi
+done
+
+for state in `ls $legislators_path` ; do
+	for json in `ls $legislators_path/$state` ; do
+		bioguide=`jq -r ".id.bioguide" "$legislators_path/$state/$json"`
+		fname=`jq -r ".name.first" "$legislators_path/$state/$json"`
+		lname=`jq -r ".name.last" "$legislators_path/$state/$json"`
+		if [ ! -f "$data_path/$bioguide.jpg" ] ; then
+			echo "No photo found for $fname $lname ($bioguide)"
+		fi
+	done
 done

39312ca97f73d76fb031d412b73819ab646babae
kokonakajima <knakajima@aclu.org>
2019-05-13 17:08:30 -0400

    assert a specific (hard-coded) number of legislators per endpoint

--- server/tests/test_legislators.py
+++ server/tests/test_legislators.py
@@ -7,13 +7,14 @@ import pytest
 # ---------------
 
 V1_ENDPOINTS = [
-	'/v1/congress/legislators'
+	('/v1/congress/legislators', 539)
 ]
 
 V2_ENDPOINTS = [
-	'/v2/congress/legislators',
-	'/v2/congress/legislators?session=115',
-	'/v2/congress/legislators?session=116'
+	('/v2/congress/legislators', 537),
+	('/v2/congress/legislators?session=115', 539),
+	('/v2/congress/legislators?session=116', 537)
+	# ('/v2/congress/legislators?session=all', 634)
 ]
 
 V1_FIELDS = [
@@ -47,6 +48,7 @@ V2_FIELDS = [
 	'term'
 ]
 
+
 # ---------------
 # Legislator data tests
 # ---------------
@@ -54,11 +56,11 @@ V2_FIELDS = [
 @pytest.mark.parametrize('endpoint',V1_ENDPOINTS+V2_ENDPOINTS)
 def test_nonzero_legislator_count(endpoint, client):
 	"""
-	Asserts a greater-than-zero number of legislators returned by each endpoint
+	Asserts the expected number of legislators returned by each endpoint
 	"""
-	response = client.get(endpoint)
+	response = client.get(endpoint[0])
 	json_data = response.get_json()
-	assert (len(json_data['congress_legislators']) > 0)
+	assert (len(json_data['congress_legislators']) == endpoint[1])
 
 
 @pytest.mark.parametrize('endpoint', V1_ENDPOINTS)
@@ -66,7 +68,7 @@ def test_v1_legislators_have_required_fields(endpoint, client):
 	"""
 	Assert that all legislators returned by a given v1 endpoint have required fields
 	"""
-	assert legislators_have_required_fields(endpoint, V1_FIELDS, client)
+	assert legislators_have_required_fields(endpoint[0], V1_FIELDS, client)
 
 
 @pytest.mark.parametrize('endpoint', V2_ENDPOINTS)
@@ -74,7 +76,7 @@ def test_v2_legislators_have_required_fields(endpoint, client):
 	"""
 	Assert that all legislators returned by a given v2 endpoint have required fields
 	"""
-	assert legislators_have_required_fields(endpoint, V2_FIELDS, client)
+	assert legislators_have_required_fields(endpoint[0], V2_FIELDS, client)
 
 
 # ---------------
@@ -98,7 +100,10 @@ def legislators_have_required_fields(endpoint, fields, client):
 				incomplete_legislators.append((legislator, field))
 
 	if len(incomplete_legislators) != 0:
-		error_message = "Endpoint %s\n\t" % (endpoint)
+		error_message = "\n----------------\n"
+		error_message += "Endpoint %s" % (endpoint)
+		error_message += "\n----------------\n"
+		error_message += "\t"
 		error_message += '\n\t'.join(map(lambda x: format_legislator_error(x[0], x[1]), incomplete_legislators))
 		raise AssertionError(error_message)
 	else:
@@ -106,5 +111,6 @@ def legislators_have_required_fields(endpoint, fields, client):
 
 def format_legislator_error(legislator, field):
 	# Todo: raise an exception if the legislator doesn't have a full name ?
-	return "Missing: %s, '%s'" % (legislator['name']['full_name'], field)
+	return "Missing '%s': %s" % (field, legislator['name']['full_name'])
+
 

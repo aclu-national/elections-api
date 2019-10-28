7b8816ec50f8c93727812df477b2b4af8d17a1eb
Dan Phiffer <dan@phiffer.org>
2019-10-28 15:22:45 -0400

    :skull: don't append scores after end of term

--- server/api/v2/congress.py
+++ server/api/v2/congress.py
@@ -448,6 +448,19 @@ def get_legislators(cur, score_filter="total", include=None, session_num=curr_se
 			legislators[aclu_id]['social'][key] = value
 
 	if score_filter == 'all':
+
+		vote_dates = {}
+		cur.execute('''
+			SELECT aclu_id, vote_date
+			FROM congress_legislator_score_index
+		''')
+		rs = cur.fetchall()
+		if rs:
+			for row in rs:
+				vote_id = row[0]
+				vote_date = arrow.get(row[1]).format('YYYY-MM-DD')
+				vote_dates[vote_id] = vote_date
+
 		cur.execute('''
 			SELECT aclu_id, session, legislator_id, position, name, value
 			FROM congress_legislator_scores
@@ -535,7 +548,10 @@ def get_legislators(cur, score_filter="total", include=None, session_num=curr_se
 					if s['session'] == session:
 						if not 'scores' in s:
 							s['scores'] = []
-						s['scores'].append(score)
+						id = score['aclu_id']
+						score['vote_date'] = vote_dates[id]
+						if score['vote_date'] <= legislators[legislator_id]['term']['end_date']:
+							s['scores'].append(score)
 
 	cur.execute('''
 		SELECT legislator_id, session
